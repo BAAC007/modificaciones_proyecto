f18ed417c20c67b0d1b2f3d487d748a515b4bc6c
Bryan Avila <bc714006@gmail.com>
2025-12-23 11:52:44 +0100

    update

--- proyecto/Chamitos_Movie_Club/front/logout.php
+++ proyecto/Chamitos_Movie_Club/front/logout.php
@@ -9,25 +9,5 @@
 en caso de que cierre la sesion.*/
 session_start();
 session_destroy();
-header("Location:login.html"); 
+header("Location:login.php"); 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>loguot</title>
-</head>
-<body>
-    <div class="buttons">
-      <button type="submit">Registrarse</button>
-      <button type="button" id="btnLogin">Iniciar sesion</button>
-      <script>
-        document.getElementById('btnLogin').addEventListener('click', function() {
-          window.location.href = 'http://localhost/MI_AREA/proyecto/Chamitos_Movie_Club/front/login.php'; 
-        });
-      </script>
-    </div>
-</body>
-</html>
